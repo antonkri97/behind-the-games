92f6c4f2d502d67be81a09e12373b446722f40cd
Anton Krivokhizhin <anton.kri97@gmail.com>
2017-08-30 19:12:43 +0300

    Added save to localstorage

--- src/index.js
+++ src/index.js
@@ -6,8 +6,39 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger'
 import users from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import throttle from 'lodash/throttle'
 
-const store = createStore(users, applyMiddleware(logger));
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.log(err);
+    return undefined;
+  }
+}
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    console.log(err);
+    return undefined;
+  }
+}
+
+const persistedState = loadState()
+const store = createStore(users, persistedState, applyMiddleware(logger));
+
+store.subscribe(throttle(() => {
+  saveState({
+    users: store.getState().users
+  });
+}, 1000));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
